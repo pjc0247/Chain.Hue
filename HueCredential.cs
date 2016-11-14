bed2feea514c8bd723fb0b1e42c8c0629d1753b8
Park June Chul <pjc0247@naver.com>
2016-11-14 23:30:40 +0900

    Create HueCredential.cs

---
+++ HueCredential.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Q42.HueApi;
+
+namespace Chain.Hue
+{
+    public class HueCredential : IServiceCredential
+    {
+        public string AppKey;
+
+        public object CreateClient()
+        {
+            var locator = new HttpBridgeLocator();
+            var bridgeIPs =
+                locator.LocateBridgesAsync(TimeSpan.FromSeconds(5)).Result;
+
+            return new LocalHueClient(bridgeIPs.First(), AppKey);
+        }
+    }
+}
